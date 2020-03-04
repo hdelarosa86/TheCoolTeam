bbb5576294c2509ed900dce89014682e3cce5200
Hector DelaRosa <hdelaros@gmail.com>
2020-03-03 21:41:43 -0500

    Lint config & linted files

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import SceneSix from './phaser/scenery/sceneSix';
 import { BattleScene, UIScene } from './phaser/battle/battle';
 
 
-//console.log(App);
+// console.log(App);
 
 export const config = {
   type: Phaser.AUTO,
@@ -22,17 +22,17 @@ export const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 0 }
-    }
+      gravity: { y: 0 },
+    },
   },
-  scene: [playGame, SceneTwo, SceneThree, SceneFour, SceneFive, SceneSix, BattleScene, UIScene]
+  scene: [playGame, SceneTwo, SceneThree, SceneFour, SceneFive, SceneSix, BattleScene, UIScene],
 };
 
 const game = new Phaser.Game(config);
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root') || document.createElement('div')
+  document.getElementById('root') || document.createElement('div'),
 );
 
-export default game
+export default game;
