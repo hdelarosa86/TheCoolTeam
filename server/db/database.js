7c5c3b61b375dfc2f4650166fc3793535408458c
Hector DelaRosa <hdelaros@gmail.com>
2022-06-03 23:35:37 -0400

    debugged db config

--- server/db/database.js
+++ server/db/database.js
@@ -10,6 +10,12 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   {
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
   }
 );
 
