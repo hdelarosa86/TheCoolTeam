94b98d51135c910b8b363b8779493cb1201ce64b
Hector DelaRosa <hdelaros@gmail.com>
2022-06-04 19:25:25 -0400

    Added dbConfig for dev/production servers to work

--- server/db/database.js
+++ server/db/database.js
@@ -4,19 +4,29 @@ const pkg = require('../../package.json');
 
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
 
+let dbConfig;
+
+if (process.env.NODE_ENV === 'production') {
+  dbConfig = {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  dbConfig = {
+    logging: false,
+  };
+}
+
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-  {
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false, // This line will fix new error
-      },
-   },
-  }
+  dbConfig
 );
 
 module.exports = db;
